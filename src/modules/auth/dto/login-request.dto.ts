2f4c954ed1beee4d094d12e874b343263a2baa71
chanyeong <jcy030896@naver.com>
2023-12-18 23:20:20 +0900

    feat: type-guard 작성

---
+++ src/modules/auth/dto/login-request.dto.ts
@@ -0,0 +1,16 @@
+import { UserType } from '@prisma/client';
+import { IsString, IsNotEmpty, IsEnum } from 'class-validator';
+
+export class LoginRequestDto {
+  @IsNotEmpty()
+  @IsString()
+  loginId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsEnum(UserType)
+  @IsNotEmpty()
+  userType: UserType;
+}
