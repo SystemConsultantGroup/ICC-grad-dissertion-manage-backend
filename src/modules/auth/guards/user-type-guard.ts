9dbf046b2e004a0a074479db9bce9850ab2906a7
chanyeong <jcy030896@naver.com>
2023-12-18 23:20:20 +0900

    feat: type-guard 작성

--- src/modules/auth/guards/user-type-guard.ts
+++ src/modules/auth/guards/user-type-guard.ts
@@ -1,10 +1,32 @@
-import { CanActivate, Injectable } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
+import {
+  CanActivate,
+  Injectable,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserType } from '@prisma/client';
+import { PrismaService } from 'src/config/database/prisma.service';
 
 @Injectable()
 export class UserTypeGuard implements CanActivate {
-    constructor(
-        private readonly reflector: Reflector,
-        private readonly prismaService: PrismaService
-    )
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly prismaService: PrismaService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const allowTypes: UserType[] = this.reflector.get(
+      'types',
+      context.getHandler(),
+    );
+
+    const { loginId, type } = request.user;
+
+    if (!allowTypes.includes(type))
+      throw new ForbiddenException('올바른 접근이 아닙니다');
+
+    return true;
+  }
 }
