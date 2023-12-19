ca8f21451ce0ae413fdeecc823fb3dfcf9c4de6b
chanyeong <jcy030896@naver.com>
2023-12-19 17:51:45 +0900

    feat: login API 작성

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginRequestDto } from './dto/login-request.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  async login(@Body() loginRequsetDto: LoginRequestDto) {
+    const accessToken = await this.authService.login(loginRequsetDto);
+
+    return new CommonResponseDto({
+      accessToken,
+    });
+  }
+}
