4526b91d523692b3e6c161442e3c6d3c6e61a56e
chanyeong <jcy030896@naver.com>
2023-12-19 16:25:48 +0900

    feat: login api 작성

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { LoginRequestDto } from './dto/login-request.dto';
+import { AuthService } from './auth.service';
+import { UseUserTypeGuard } from './decorators/user-type-guard.decorator';
+import { UserType } from '../../common/enums/user-type.enum';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  async login(@Body() loginRequestDto: LoginRequestDto) {
+    return await this.authService.login(loginRequestDto);
+  }
+
+  @UseUserTypeGuard([UserType.ADMIN])
+  @Get()
+}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../../config/database/prisma.service';
+import { LoginRequestDto } from './dto/login-request.dto';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+  ) {}
+
+  async login(loginRequsetDto: LoginRequestDto) {
+    const user: User = await this.prisma.user.findUnique({
+      where: {
+        loginId: loginRequsetDto.loginId,
+      },
+    });
+
+    if (
+      !user ||
+      !(await bcrypt.compare(loginRequsetDto.password, user.password))
+    ) {
+      throw new UnauthorizedException('학번이나 비밀번호가 올바르지 않습니다');
+    }
+
+    return this.jwt.sign({
+      loginId: user.loginId,
+      type: user.type,
+    });
+  }
+
+  createHash(password: string) {
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    return bcrypt.hashSync(password, salt);
+  }
+}
