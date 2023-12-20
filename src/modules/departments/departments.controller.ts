b9f06f8ba93d9c729eb20f732ba30f593f6025db
R3gardless <pidaoh@g.skku.edu>
2023-12-20 22:56:17 +0900

    [FEAT] department resource 생성

---
+++ src/modules/departments/departments.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { DepartmentsService } from './departments.service';
+
+@Controller('departments')
+export class DepartmentsController {
+  constructor(private readonly departmentsService: DepartmentsService) {}
+}
