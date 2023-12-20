729c6759a351876ce7dcf88193ee30a0cc10c472
R3gardless <pidaoh@g.skku.edu>
2023-12-20 22:56:17 +0900

    [FEAT] department resource 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AppConfigModule } from './config/app/config.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { MinioClientModule } from './config/file/minio-client.module';
+import { DepartmentsModule } from './modules/departments/departments.module';
 
 @Module({
-  imports: [AppConfigModule, AuthModule, MinioClientModule],
+  imports: [AppConfigModule, AuthModule, MinioClientModule, DepartmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
