296bf7dedfe785b32fe12194039d9ad04e6d1157
R3gardless <pidaoh@g.skku.edu>
2023-12-22 17:31:45 +0900

    [FEAT] department controller & service 기본 로직 구현

--- src/modules/departments/departments.controller.ts
+++ src/modules/departments/departments.controller.ts
@@ -1,7 +1,47 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from "@nestjs/common";
 import { DepartmentsService } from "./departments.service";
+import { ApiBadRequestResponse, ApiInternalServerErrorResponse, ApiOperation, ApiUnauthorizedResponse } from "@nestjs/swagger";
+import { CommonResponseDto } from "src/common/dtos/common-response.dto";
+import { CreateDepartmentDto } from "./dtos/create-deparment.dto";
 
 @Controller("departments")
 export class DepartmentsController {
   constructor(private readonly departmentsService: DepartmentsService) {}
+  
+  @Get("")
+  @ApiOperation({
+    summary: "Get all departments",
+    description: "Get all departments",
+  })
+  @ApiUnauthorizedResponse({ description: "Unauthorized" })
+  @ApiBadRequestResponse({ description: "Bad Request" })
+  @ApiInternalServerErrorResponse({ description: "Internal Server Error" })
+  async getAllDepartments() {
+    return new CommonResponseDto(await this.departmentsService.getAllDepartments());
+  }
+
+  @Post("")
+  @ApiOperation({
+    summary: "Create a department",
+    description: "Create a department",
+  })
+  @ApiUnauthorizedResponse({ description: "Unauthorized" })
+  @ApiBadRequestResponse({ description: "Bad Request" })
+  @ApiInternalServerErrorResponse({ description: "Internal Server Error" })
+  async createDepartment(@Body() createDepartmentDto: CreateDepartmentDto) {
+    return new CommonResponseDto(await this.departmentsService.createDepartment(createDepartmentDto));
+  } 
+
+  @Delete(":id")
+  @ApiOperation({
+    summary: "Delete a department",
+    description: "Delete a department",
+  })
+  @ApiUnauthorizedResponse({ description: "Unauthorized" })
+  @ApiBadRequestResponse({ description: "Bad Request" })
+  @ApiInternalServerErrorResponse({ description: "Internal Server Error" })
+  async deleteDepartment(@Param("id", ParseIntPipe) id: number) {
+    return new CommonResponseDto(await this.departmentsService.deleteDepartment(id));
+  }
 }
+
