2a28f013b481e8bf9ce8cdb7c0aed6c75e76b33a
chanyeong <jcy030896@naver.com>
2023-12-23 15:03:52 +0900

    fix: 누락된 auth.controller 수정

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { LoginRequestDto } from "./dto/login-request.dto";
+import { CommonResponseDto } from "../../common/dtos/common-response.dto";
+import { UseUserTypeGuard } from "./decorators/user-type-guard.decorator";
+import { UserType } from "@prisma/client";
 
 @Controller("auth")
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post()
+  async login(@Body() loginRequsetDto: LoginRequestDto) {
+    const accessToken = await this.authService.login(loginRequsetDto);
+    return new CommonResponseDto({
+      accessToken,
+    });
+  }
+
+  @UseUserTypeGuard([UserType.ADMIN])
+  @Get(":id")
+  async loginUser(@Param("id") id: string) {
+    const accessToken = await this.authService.loginUser(id);
+
+    return new CommonResponseDto({
+      accessToken,
+    });
+  }
+}
