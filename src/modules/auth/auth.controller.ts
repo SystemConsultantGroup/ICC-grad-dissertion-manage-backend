e4c565cc4749b3c75bde2c28fe5c77f4db1c7935
chanyeong <jcy030896@naver.com>
2023-12-24 16:31:23 +0900

    feat: login API 관련 swagger 작성

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,11 +4,21 @@ import { LoginRequestDto } from "./dto/login-request.dto";
 import { CommonResponseDto } from "../../common/dtos/common-response.dto";
 import { UseUserTypeGuard } from "./decorators/user-type-guard.decorator";
 import { UserType } from "@prisma/client";
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { LoginResponseDto } from "./dto/login-response.dto";
 
+@ApiTags("Auth(로그인) API")
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post()
+  @ApiOperation({ summary: "로그인" })
+  @ApiBody({ type: LoginRequestDto })
+  @ApiResponse({
+    description: "로그인 성공",
+    type: LoginResponseDto,
+    status: 201,
+  })
   async login(@Body() loginRequsetDto: LoginRequestDto) {
     const accessToken = await this.authService.login(loginRequsetDto);
     return new CommonResponseDto({
