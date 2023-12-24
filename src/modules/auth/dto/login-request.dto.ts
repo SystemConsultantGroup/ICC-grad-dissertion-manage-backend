bb751c476a8b45785744b5e1dba367ef28416f42
chanyeong <jcy030896@naver.com>
2023-12-24 16:31:23 +0900

    feat: login API 관련 swagger 작성

--- src/modules/auth/dto/login-request.dto.ts
+++ src/modules/auth/dto/login-request.dto.ts
@@ -1,16 +1,14 @@
-import { UserType } from "@prisma/client";
-import { IsString, IsNotEmpty, IsEnum } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsNotEmpty } from "class-validator";
 
 export class LoginRequestDto {
+  @ApiProperty({ example: "admin" })
   @IsNotEmpty()
   @IsString()
   loginId: string;
 
+  @ApiProperty({ example: "123" })
   @IsNotEmpty()
   @IsString()
   password: string;
-
-  @IsEnum(UserType)
-  @IsNotEmpty()
-  type: UserType;
 }
