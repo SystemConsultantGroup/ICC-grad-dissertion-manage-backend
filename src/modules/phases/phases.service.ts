2498bee5db1cf092d7d6494a07a7d6da43c2328d
chanyeong <jcy030896@naver.com>
2023-12-25 14:53:17 +0900

    feat: updatePhase API 작성

--- src/modules/phases/phases.service.ts
+++ src/modules/phases/phases.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { PrismaService } from "../../config/database/prisma.service";
 import { getKST } from "../../common/utils/date.util";
+import { UpdatePhaseDto } from "./dtos/update-phase.dto";
 
 @Injectable()
 export class PhasesService {
@@ -39,5 +40,41 @@ export class PhasesService {
     });
 
     return phase;
+  }
+
+  async updatePhase(id: number, updatePhaseDto: UpdatePhaseDto) {
+    const phase = this.prismaService.phase.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!phase) {
+      throw new BadRequestException("존재하지 않는 시스템 단계입니다.");
+    }
+
+    if (updatePhaseDto.end <= updatePhaseDto.start) {
+      throw new BadRequestException("기간이 잘못 설정되었습니다.");
+    }
+
+    const endDateTime = new Date(updatePhaseDto.end);
+    endDateTime.setUTCHours(23, 59, 59, 0);
+    try {
+      return await this.prismaService.$transaction(async (tx) => {
+        await tx.phase.update({
+          where: {
+            id,
+          },
+          data: {
+            start: updatePhaseDto.start,
+            end: endDateTime.toISOString(),
+          },
+        });
+
+        // cron-job task 설정 추가가 필요합니다.
+      });
+    } catch (error) {
+      throw new BadRequestException("invalid request");
+    }
   }
 }
