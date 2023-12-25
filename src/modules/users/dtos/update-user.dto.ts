ee07552108673163d3912defb9f668f4cca8d832
yesjuhee <juhee0924@g.skku.edu>
2023-12-25 16:23:13 +0900

    chore: UpdateUserDto example 추가

--- src/modules/users/dtos/update-user.dto.ts
+++ src/modules/users/dtos/update-user.dto.ts
@@ -2,20 +2,20 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class UpdateUserDto {
-  @ApiProperty({ description: "수정할 이메일" })
+  @ApiProperty({ description: "수정할 이메일", example: "email@gmail.com" })
   @IsOptional()
   @IsNotEmpty()
   @IsString()
   @IsEmail()
   email: string;
 
-  @ApiProperty({ description: "수정할 비밀번호" })
+  @ApiProperty({ description: "수정할 비밀번호", example: "1111" })
   @IsOptional()
   @IsNotEmpty()
   @IsString()
   password: string;
 
-  @ApiProperty({ description: "수정할 전화번호" })
+  @ApiProperty({ description: "수정할 전화번호", example: "01098769876" })
   @IsOptional()
   @IsNotEmpty()
   @IsString()
