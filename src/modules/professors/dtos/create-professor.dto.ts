0ba9832f8d0c4fc0e067f36e786c8fe536ac7723
R3gardless <pidaoh@g.skku.edu>
2023-12-26 21:21:41 +0900

    [FEAT] professor 조회 및 생성 구현

---
+++ src/modules/professors/dtos/create-professor.dto.ts
@@ -0,0 +1,58 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateProfessorDto {
+  @ApiProperty({
+    description: "로그인 아이디",
+    example: "prof1",
+  })
+  @IsNotEmpty({ message: "로그인 아이디를 입력해주세요." })
+  @IsString()
+  @Type(() => String)
+  loginId: string;
+
+  @ApiProperty({
+    description: "비밀번호",
+    example: "1234",
+  })
+  @IsNotEmpty({ message: "비밀번호를 입력해주세요." })
+  @IsString()
+  @Type(() => String)
+  password: string;
+
+  @ApiProperty({
+    description: "이름",
+    example: "이교수",
+  })
+  @IsNotEmpty({ message: "이름을 입력해주세요." })
+  @IsString()
+  @Type(() => String)
+  name: string;
+
+  @ApiProperty({
+    description: "이메일",
+    example: "skku@skku.edu",
+  })
+  @IsNotEmpty({ message: "이메일을 입력해주세요." })
+  @IsString()
+  @Type(() => String)
+  email: string;
+
+  @ApiProperty({
+    description: "연락처",
+    example: "010-1234-5678",
+  })
+  @IsNotEmpty({ message: "연락처를 입력해주세요." })
+  @IsString()
+  @Type(() => String)
+  phone: string;
+
+  @ApiProperty({
+    description: "학과 아이디",
+    example: 1,
+  })
+  @IsNotEmpty({ message: "학과 아이디를 입력해주세요." })
+  @Type(() => Number)
+  deptId: number;
+}
