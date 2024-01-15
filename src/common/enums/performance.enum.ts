d969b3199e523564a4e8d1f08dd9b4027270b664
chanyeong <jcy030896@naver.com>
2024-01-15 18:04:38 +0900

    feat: 논문실적 관련 enum 작성

---
+++ src/common/enums/performance.enum.ts
@@ -0,0 +1,16 @@
+export enum Performance {
+  SCI = "SCI",
+  SCOPUS = "SCOPUS",
+  SCIE = "SCIE",
+  INTERNATIONAL_B = "INTERNATIONAL_B",
+  DOMESTIC_A = "DOMESTIC_A",
+  DOMESTIC_B = "DOMESTIC_B",
+  ICOP = "ICOP",
+  ICP = "ICP",
+  DCOP = "DCOP",
+  DCP = "DCP",
+  IPR = "IPR",
+  IPA = "IPA",
+  DPR = "DPR",
+  DPA = "DPA",
+}
