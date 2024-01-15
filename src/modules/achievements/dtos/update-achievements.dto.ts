21d131c51417051a0ff0ee5dfa37d299d695d1bf
chanyeong <jcy030896@naver.com>
2024-01-15 18:05:08 +0900

    feat: 논문실적 create, update dto 작성

---
+++ src/modules/achievements/dtos/update-achievements.dto.ts
@@ -0,0 +1,78 @@
+import { IsDate, IsEnum, IsNotEmpty, IsOptional, IsPositive, IsString } from "class-validator";
+import { Performance } from "../../../common/enums/performance.enum";
+import { Author } from "../../../common/enums/author.enum";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class UpdateAchievementsDto {
+  @ApiProperty({
+    description: "논문 실적 id",
+    example: "1",
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsPositive()
+  achievementId: number;
+
+  @ApiProperty({
+    description: "논문 실적 구분",
+    example: "SCI",
+    required: false,
+  })
+  @IsOptional()
+  @IsEnum(Performance)
+  performance: Performance;
+
+  @ApiProperty({
+    description: "논문 제목",
+    example: "Design of Real-Time SIFT Feature Extraction",
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  paperTitle: string;
+
+  @ApiProperty({
+    description: "학술지명/학술대회명",
+    example: "ACM",
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  journalName: string;
+
+  @ApiProperty({
+    description: "ISSN",
+    example: "1234-1234",
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  ISSN: string;
+
+  @ApiProperty({
+    description: "게재년월",
+    example: "2023-01-13",
+    required: false,
+  })
+  @IsOptional()
+  @IsDate()
+  publicationDate: Date;
+
+  @ApiProperty({
+    description: "주저자여부",
+    example: "FIRST_AUTHOR",
+    required: false,
+  })
+  @IsOptional()
+  @IsEnum(Author)
+  authorType: Author;
+
+  @ApiProperty({
+    description: "저자수",
+    example: " 2",
+    required: false,
+  })
+  @IsOptional()
+  @IsPositive()
+  authorNumbers: number;
+}
