8f12d91d336d5525b17c6eac00fa08c36cf653d6
chanyeong <jcy030896@naver.com>
2024-01-16 20:16:49 +0900

    feat: 논문실적 생성, 조회, 업데이트 api 작성

--- src/modules/achievements/achievements.service.ts
+++ src/modules/achievements/achievements.service.ts
@@ -1,4 +1,118 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { PrismaService } from "../../config/database/prisma.service";
+import { CreateAchievementsDto } from "./dtos/create-achievements.dto";
+import { UpdateAchievementsDto } from "./dtos/update-achievements.dto";
+import { AchievementsSearchQuery } from "./dtos/achievements-query.dto";
+import { User, UserType } from "@prisma/client";
 
 @Injectable()
-export class AchievementsService {}
+export class AchievementsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createAchievement(userId: number, createAchievementsDto: CreateAchievementsDto) {
+    const { performance, paperTitle, journalName, ISSN, publicationDate, authorType, authorNumbers } =
+      createAchievementsDto;
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) throw new BadRequestException("올바르지 않은 유저id입니다.");
+    return await this.prismaService.achievements.create({
+      data: {
+        userId,
+        performance,
+        paperTitle,
+        journalName,
+        ISSN,
+        publicationDate,
+        authorNumbers,
+        authorType,
+      },
+    });
+  }
+
+  async updateAchievement(updateAchievementDto: UpdateAchievementsDto) {
+    const { achievementId, performance, paperTitle, journalName, ISSN, publicationDate, authorType, authorNumbers } =
+      updateAchievementDto;
+    try {
+      return await this.prismaService.achievements.update({
+        where: {
+          id: achievementId,
+        },
+        data: {
+          ...(performance && { performance }),
+          ...(paperTitle && { paperTitle }),
+          ...(journalName && { journalName }),
+          ...(ISSN && { ISSN }),
+          ...(publicationDate && { publicationDate }),
+          ...(authorType && { authorType }),
+          ...(authorNumbers && { authorNumbers }),
+        },
+      });
+    } catch {
+      throw new InternalServerErrorException("논문 실적 업데이트에 실패했습니다.");
+    }
+  }
+
+  async getAchievements(currentUser: User, achievementsQuery: AchievementsSearchQuery) {
+    if (currentUser.type == UserType.STUDENT) {
+      const studentQuery = {
+        where: {
+          userId: currentUser.id,
+        },
+        include: {
+          User: {
+            include: {
+              department: true,
+            },
+          },
+        },
+        skip: achievementsQuery.getOffset(),
+        take: achievementsQuery.getLimit(),
+      };
+
+      try {
+        const [achievements, counts] = await this.prismaService.$transaction([
+          this.prismaService.achievements.findMany(studentQuery),
+          this.prismaService.achievements.count({ where: studentQuery.where }),
+        ]);
+        return { achievements, counts };
+      } catch (e) {
+        throw new InternalServerErrorException(e);
+      }
+    }
+
+    const { major, author, paperTitle, performance, journalName, publicationDate } = achievementsQuery;
+
+    const query = {
+      where: {
+        ...(major && { User: { department: { name: { contains: major } } } }),
+        ...(author && { User: { name: { contains: author } } }),
+        ...(paperTitle && { paperTitle: { contains: paperTitle } }),
+        ...(performance && { performance: { equals: performance } }),
+        ...(journalName && { journalName: { contains: journalName } }),
+        ...(publicationDate && { publicationDate: { equals: publicationDate } }),
+      },
+      include: {
+        User: {
+          include: {
+            department: true,
+          },
+        },
+      },
+
+      skip: achievementsQuery.getOffset(),
+      take: achievementsQuery.getLimit(),
+    };
+    try {
+      const [achievements, counts] = await this.prismaService.$transaction([
+        this.prismaService.achievements.findMany(query),
+        this.prismaService.achievements.count({ where: query.where }),
+      ]);
+      return { achievements, counts };
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
+  }
+}
