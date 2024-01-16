f3e729e5edb190baa5d221bd78e5b3d57b954ef6
chanyeong <jcy030896@naver.com>
2024-01-17 00:39:21 +0900

    fix: 논문 실적 업데이트 로직 수정

--- src/modules/achievements/achievements.controller.ts
+++ src/modules/achievements/achievements.controller.ts
@@ -1,7 +1,17 @@
 import { Body, Controller, Get, Param, Post, Query, UseGuards } from "@nestjs/common";
 import { AchievementsService } from "./achievements.service";
 import { PositiveIntPipe } from "../../common/pipes/positive-int.pipe";
-import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+  getSchemaPath,
+} from "@nestjs/swagger";
 import { JwtGuard } from "../auth/guards/jwt.guard";
 import { CreateAchievementsDto } from "./dtos/create-achievements.dto";
 import { UseUserTypeGuard } from "../auth/decorators/user-type-guard.decorator";
@@ -24,17 +34,34 @@ export class AchievementsController {
     description: "논문실적 등록",
   })
   @UseUserTypeGuard([UserType.STUDENT])
-  @Post(":userId")
+  @Post(":id")
+  @ApiCreatedResponse({
+    description: '논문 실적 등록 성공',
+    type: CommonResponseDto
+  })
+  @ApiBadRequestResponse({
+    description: '존재하지 않는 유저의 논문 실적을 등록하려고 하였습니다'
+  })
+  @ApiUnauthorizedResponse({
+    description: '학생 권한 접근 가능'
+  })
   async createAchievement(
-    @Param("userId", PositiveIntPipe) userId,
+    @Param("id", PositiveIntPipe) id,
     @Body() createAchievementsDto: CreateAchievementsDto
   ) {
-    await this.achievemenstService.createAchievement(userId, createAchievementsDto);
+    await this.achievemenstService.createAchievement(id, createAchievementsDto);
   }
 
   @ApiOperation({
     summary: "논문실적 조회",
-    description: "논문실적 조회",
+    description: "관리자와 학생모두 이 api를 통해 논문실적을 조회합니다.",
+  })
+  @ApiOkResponse({
+    description: "논문 실적 조회 성공",
+    type: [AchievementDto],
+  })
+  @ApiInternalServerErrorResponse({
+    description: "논문 조회 실패",
   })
   @UseUserTypeGuard([UserType.ADMIN, UserType.STUDENT])
   @Get()
