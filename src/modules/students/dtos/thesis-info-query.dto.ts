95f3d857a21e2aa8d72a1ddab66d70bc2bb25f07
yesjuhee <juhee0924@g.skku.edu>
2024-01-16 15:49:12 +0900

    fix: 학생 논문 정보 조회 API 수정(수정지시사항단계 반영)

--- src/modules/students/dtos/thesis-info-query.dto.ts
+++ src/modules/students/dtos/thesis-info-query.dto.ts
@@ -2,13 +2,14 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsEnum, IsNotEmpty, IsString } from "class-validator";
 
 export enum ThesisQueryType {
-  PRE = "pre",
+  PRELIMINARY = "pre",
   MAIN = "main",
+  REVISION = "rivision",
   NOW = "now",
 }
 
 export class ThesisInfoQueryDto {
-  @ApiProperty({ description: "논문 정보 타입 선택", example: "now", enum: ["now", "pre", "main"] })
+  @ApiProperty({ description: "논문 정보 타입 선택", example: "now", enum: ["now", "pre", "main", "rivision"] })
   @IsNotEmpty()
   @IsString()
   @IsEnum(ThesisQueryType)
