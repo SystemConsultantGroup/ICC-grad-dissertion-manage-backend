4c37554628397b01ddf82bbc33daf3d5decaa396
chanyeong <jcy030896@naver.com>
2024-01-17 17:08:16 +0900

    feat: 논문실적 엑셀 다운로드 로직 작성

--- src/modules/achievements/achievements.service.ts
+++ src/modules/achievements/achievements.service.ts
@@ -2,9 +2,11 @@ import { BadRequestException, Injectable, InternalServerErrorException } from "@
 import { PrismaService } from "../../config/database/prisma.service";
 import { CreateAchievementsDto } from "./dtos/create-achievements.dto";
 import { UpdateAchievementsDto } from "./dtos/update-achievements.dto";
-import { AchievementsSearchQuery } from "./dtos/achievements-query.dto";
+import { AchievementsExcelQuery, AchievementsSearchQuery } from "./dtos/achievements-query.dto";
 import { User, UserType } from "@prisma/client";
-
+import * as XLSX from "xlsx";
+import * as DateUtil from "../../common/utils/date.util";
+import { Readable } from "stream";
 @Injectable()
 export class AchievementsService {
   constructor(private readonly prismaService: PrismaService) {}
@@ -120,5 +122,58 @@ export class AchievementsService {
     } catch (e) {
       throw new InternalServerErrorException(e);
     }
+  }
+
+  async getAchievementsExcel(achievementsQuery: AchievementsExcelQuery) {
+    const { major, author, paperTitle, performance, journalName, publicationDate } = achievementsQuery;
+
+    const achievements = await this.prismaService.achievements.findMany({
+      where: {
+        ...(major && { User: { department: { name: { contains: major } } } }),
+        ...(author && { User: { name: { contains: author } } }),
+        ...(paperTitle && { paperTitle: { contains: paperTitle } }),
+        ...(performance && { performance: { equals: performance } }),
+        ...(journalName && { journalName: { contains: journalName } }),
+        ...(publicationDate && { publicationDate: { equals: publicationDate } }),
+      },
+      include: {
+        User: {
+          include: {
+            department: true,
+          },
+        },
+      },
+    });
+    if (!achievements) throw new BadRequestException("검색된 논문 실적이 없습니다.");
+    const records = achievements.map((achievement) => {
+      const record = {};
+      const student = achievement.User;
+      const dept = achievement.User.department;
+
+      record["학번"] = student.loginId;
+      record["이름"] = student.name;
+      record["학과"] = dept.name;
+
+      record["실적 구분"] = achievement.performance;
+      record["학술지 또는 학술대회명"] = achievement.journalName;
+      record["논문 제목"] = achievement.paperTitle;
+      (record["ISSN"] = achievement.ISSN ? achievement.ISSN : "미제출"),
+        (record["게재년월일"] = achievement.publicationDate);
+      record["주저자여부"] = achievement.authorType;
+      record["저자수"] = achievement.userId;
+
+      return record;
+    });
+
+    const workSheet = XLSX.utils.json_to_sheet(records);
+    const workBook = XLSX.utils.book_new();
+    const fileName = "논문실적리스트_" + DateUtil.getCurrentTime().fullDateTime + ".xlsx";
+    XLSX.utils.book_append_sheet(workBook, workSheet, "논문실적 목록");
+
+    const stream = new Readable();
+    stream.push(await XLSX.write(workBook, { type: "buffer" }));
+    stream.push(null);
+
+    return { fileName, stream };
   }
 }
