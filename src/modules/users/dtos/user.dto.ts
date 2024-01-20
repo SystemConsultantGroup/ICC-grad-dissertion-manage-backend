2d572d5be7f1654bfd8563c4ae568292ac561af8
yesjuhee <juhee0924@g.skku.edu>
2024-01-20 23:34:31 +0900

    feat: 교수 서명 업로드 추가

--- src/modules/users/dtos/user.dto.ts
+++ src/modules/users/dtos/user.dto.ts
@@ -1,11 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Department, User, UserType } from "@prisma/client";
+import { Department, File, Process, Stage, User, UserType } from "@prisma/client";
 import { DepartmentDto } from "src/modules/departments/dtos/department.dto";
+import { FileDto } from "src/modules/files/dtos/file.dto";
 
 export class UserDto {
   constructor(
     studentData: Partial<User> & {
       department: Partial<Department>;
+      studentProcess?: Partial<Process>;
+      signFile?: File;
     }
   ) {
     this.id = studentData.id;
@@ -14,7 +17,9 @@ export class UserDto {
     this.email = studentData.email;
     this.phone = studentData.phone;
     this.type = studentData.type;
-    this.department = new DepartmentDto(studentData.department);
+    this.department = studentData.department ? new DepartmentDto(studentData.department) : undefined;
+    this.stage = studentData.studentProcess?.currentPhase;
+    this.signFile = studentData.signFile ? new FileDto(studentData.signFile) : undefined;
   }
 
   @ApiProperty({ description: "아이디" })
@@ -29,6 +34,10 @@ export class UserDto {
   phone: string;
   @ApiProperty({ description: "사용자 유형", enum: UserType })
   type: UserType;
-  @ApiProperty({ description: "학과", type: () => DepartmentDto })
+  @ApiProperty({ description: "학과" })
   department: DepartmentDto;
+  @ApiProperty({ description: "시스템 단계", enum: Stage })
+  stage?: Stage;
+  @ApiProperty({ description: "교수 서명 파일" })
+  signFile?: FileDto;
 }
