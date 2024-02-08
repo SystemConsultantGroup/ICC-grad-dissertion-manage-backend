5cf663f88f011d7c4683c2de70d33646e2defc11
yesjuhee <juhee0924@g.skku.edu>
2024-02-08 17:27:11 +0900

    fix: department dto 에 modificationFlag 추가

--- src/modules/departments/dtos/department.dto.ts
+++ src/modules/departments/dtos/department.dto.ts
@@ -5,10 +5,13 @@ export class DepartmentDto {
   constructor(departmentData: Partial<Department>) {
     this.id = departmentData.id;
     this.name = departmentData.name;
+    this.modificationFlag = departmentData.modificationFlag;
   }
 
   @ApiProperty({ description: "학과 아이디" })
   id: number;
   @ApiProperty({ description: "학과 이름" })
   name: string;
+  @ApiProperty({ description: "수정지시사항 해당 학과 여부" })
+  modificationFlag: boolean;
 }
