d916a058b9d58eeb7fd6d172ce58282dbaa7f925
김현석 <wefwef12e@naver.com>
2024-04-29 17:20:04 +0900

    chore: 서명 없을때 예외처리 (#106)

--- src/modules/reviews/reviews.service.ts
+++ src/modules/reviews/reviews.service.ts
@@ -165,9 +165,13 @@ export class ReviewsService {
                   for (const key of Object.keys(slot)) formatHtml = formatHtml.replace(key, slot[key]);
                 }
               } else if (key == "$서명") {
-                const readable = await this.minioClientService.getFile(replacer[key]);
-                const buf = await readableToBuffer(readable);
-                formatHtml = formatHtml.replace(key, `data:image/png;base64, ${buf.toString("base64")}`);
+                if (replacer[key]) {
+                  const readable = await this.minioClientService.getFile(replacer[key]);
+                  const buf = await readableToBuffer(readable);
+                  formatHtml = formatHtml.replace(key, `data:image/png;base64, ${buf.toString("base64")}`);
+                } else {
+                  formatHtml = formatHtml.replace(key, "");
+                }
               } else {
                 formatHtml = formatHtml.replace(key, replacer[key]);
               }
@@ -295,9 +299,13 @@ export class ReviewsService {
           const replacerKeys = Object.keys(replacer);
           for (const key of replacerKeys) {
             if (key == "$서명") {
-              const readable = await this.minioClientService.getFile(replacer[key]);
-              const buf = await readableToBuffer(readable);
-              formatHtml = formatHtml.replace(key, `data:image/png;base64, ${buf.toString("base64")}`);
+              if (replacer[key]) {
+                const readable = await this.minioClientService.getFile(replacer[key]);
+                const buf = await readableToBuffer(readable);
+                formatHtml = formatHtml.replace(key, `data:image/png;base64, ${buf.toString("base64")}`);
+              } else {
+                formatHtml = formatHtml.replace(key, "");
+              }
             } else {
               formatHtml = formatHtml.replaceAll(key, replacer[key]);
             }
