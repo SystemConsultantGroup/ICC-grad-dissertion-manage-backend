04b2a939b428882b10bd3c74fc65df69a34bf1e2
chanyeong <jcy030896@naver.com>
2024-05-09 01:14:29 +0900

    fix: 누락된 query 조건 추가

--- src/modules/reviews/reviews.service.ts
+++ src/modules/reviews/reviews.service.ts
@@ -697,7 +697,7 @@ export class ReviewsService {
       return file.fileId == null;
     });
     if (submitted.length != 0) {
-      throw new BadRequestException("논문 제출이 완료되지 않은 학생은 심사할 수 없습니다.");
+      throw new BadRequestException("미제출파일이 있는 학생은 심사할 수 없습니다.");
     }
     if (!foundReview) throw new NotFoundException("존재하지 않는 심사정보입니다");
     if (userType == UserType.PROFESSOR && foundReview.reviewerId != userId) {
@@ -1080,6 +1080,7 @@ export class ReviewsService {
     const otherReviews = await this.prismaService.review.findMany({
       where: {
         thesisInfoId: foundReview.thesisInfoId,
+        isFinal: true,
       },
     });
     const notSubmitted = otherReviews.filter((review) => {
