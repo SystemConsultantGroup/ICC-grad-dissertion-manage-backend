b5b8db1638f4852e3b7f2f6dbc04798dea8e75d7
chanyeong <jcy030896@naver.com>
2024-08-08 12:33:32 +0900

    feat: query logging 기능 추가

--- src/config/database/prisma.service.ts
+++ src/config/database/prisma.service.ts
@@ -2,6 +2,17 @@ import { Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: [
+        { emit: "event", level: "query" },
+        { emit: "stdout", level: "info" },
+        { emit: "stdout", level: "warn" },
+        { emit: "stdout", level: "error" },
+      ],
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
