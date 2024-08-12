517b5e394f586c86c94aeadf21207f69b2abaae0
chanyeong <jcy030896@naver.com>
2024-08-12 15:16:13 +0900

    fix: puppeteer timeout 제거 & 동작시간 로깅 추가

--- src/common/utils/convert-html-to-pdf.ts
+++ src/common/utils/convert-html-to-pdf.ts
@@ -6,6 +6,11 @@ export const convertHTMLToPDF = async (html, callback, options = null, puppeteer
       "Invalid Argument: HTML expected as type of string and received a value of a different type. Check your request body and request headers."
     );
   }
+
+  let startTime, endTime, elapsedTime;
+
+  // Puppeteer launch time
+  startTime = Date.now();
   let browser;
   if (puppeteerArgs) {
     browser = await puppeteer.launch(puppeteerArgs);
@@ -20,23 +25,52 @@ export const convertHTMLToPDF = async (html, callback, options = null, puppeteer
       ],
     });
   }
+  endTime = Date.now();
+  elapsedTime = endTime - startTime;
+  console.log(`Browser launched in ${elapsedTime} ms`);
 
+  // New page creation time
+  startTime = Date.now();
   const page = await browser.newPage();
+  endTime = Date.now();
+  elapsedTime = endTime - startTime;
+  console.log(`New page created in ${elapsedTime} ms`);
+
   if (!options) {
     options = { width: "16.5in", height: "23.4in", preferCSSPageSize: false, printBackground: true };
   }
 
+  // Page content setting or navigation time
   if (remoteContent === true) {
+    startTime = Date.now();
     await page.goto(`data:text/html;base64,${Buffer.from(html).toString("base64")}`, {
       waitUntil: "networkidle0",
+      timeout: 0,
     });
+    endTime = Date.now();
+    elapsedTime = endTime - startTime;
+    console.log(`Page navigation completed in ${elapsedTime} ms`);
   } else {
-    //page.setContent will be faster than page.goto if html is a static
+    startTime = Date.now();
     await page.setContent(html);
+    endTime = Date.now();
+    elapsedTime = endTime - startTime;
+    console.log(`Page content set in ${elapsedTime} ms`);
   }
 
+  // PDF generation time
+  startTime = Date.now();
   await page.pdf(options).then(callback, function (error) {
     console.log(error);
   });
+  endTime = Date.now();
+  elapsedTime = endTime - startTime;
+  console.log(`PDF generated in ${elapsedTime} ms`);
+
+  // Browser close time
+  startTime = Date.now();
   await browser.close();
+  endTime = Date.now();
+  elapsedTime = endTime - startTime;
+  console.log(`Browser closed in ${elapsedTime} ms`);
 };
